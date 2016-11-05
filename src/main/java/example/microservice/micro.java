6300d3017f3dff9800c9b5f7dabcc7a883181012
sala_c <sala_c@10.180.12.22>
2016-11-05 11:45:50 -0500

    Servicio Colpatria

--- src/main/java/example/microservice/micro.java
+++ src/main/java/example/microservice/micro.java
@@ -1,12 +1,16 @@
 package example.microservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService(name="miServicio")
 public class micro {
-
+	List<String> listadoPlacas = new ArrayList<String>();
+	
 	@WebMethod()
 	public String sayHello(@WebParam(name="nombre") String name) {
 	    System.out.println("Hello: " + name);
@@ -17,5 +21,23 @@ public class micro {
 	public int sumar(@WebParam(name="valor1") int a,@WebParam(name="valor2") int b) {
 	    System.out.println("sumar: " + (a+b));
 	    return (a+b);
+	}
+	
+	@WebMethod()
+	public List<String> listPlacaRobadas() {		
+		listadoPlacas.add("Placa1");
+		listadoPlacas.add("Placa2");
+		listadoPlacas.add("Placa3");
+		return listadoPlacas;
+	}
+	@WebMethod()
+	public Boolean isPlacaRobada(String placa) {
+		Boolean result = false;
+		for (String pl : listPlacaRobadas()) {
+			if (pl.equals(placa)) {
+				result = true;
+			}			
+		}		
+		return result;
 	}
 }
